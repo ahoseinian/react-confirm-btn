e4060328f99b198ac82903244944e61fa9a05292
Amir Hoseinian <amir.anyware@gmail.com>
2017-06-08 21:47:47 +0430

    Add new styles on stories

--- stories/index.js
+++ stories/index.js
@@ -4,15 +4,31 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import ConfirmBtn from '../lib/ConfirmBtn';
 import { action } from '@storybook/addon-actions';
+import './styles.css';
 
 const s = {
-  normal: {
-    backgroundColor: 'CornflowerBlue',
+  withStyle: {
+    backgroundColor: 'Tomato',
+    color: 'white',
+    border: 'none',
+    padding: '1rem 2rem',
+    cursor: 'pointer',
   },
 };
 
-storiesOf('CofirmBtn', module).add('normal', () =>
-  <ConfirmBtn style={s.normal} test="22" onConfirm={action('Confirmed!!!')}>
-    Delete
-  </ConfirmBtn>,
-);
+storiesOf('CofirmBtn', module)
+  .add('normal', () =>
+    <ConfirmBtn onConfirm={action('Confirmed!!!')}>
+      Delete
+    </ConfirmBtn>,
+  )
+  .add('with style', () =>
+    <ConfirmBtn style={s.withStyle} onConfirm={action('Confirmed!!!')}>
+      Delete
+    </ConfirmBtn>,
+  )
+  .add('with className', () =>
+    <ConfirmBtn className="btn" onConfirm={action('Confirmed!!!')}>
+      Delete
+    </ConfirmBtn>,
+  );
