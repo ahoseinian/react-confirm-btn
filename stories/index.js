5ac4be35325cd37d3f6bfb17f4d208a75c1b0305
Amir Hoseinian <amir.anyware@gmail.com>
2017-06-08 17:00:17 +0430

    Add onConfirm action story

--- stories/index.js
+++ stories/index.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import ConfirmBtn from '../lib/ConfirmBtn';
+import { action } from '@storybook/addon-actions';
 
 const s = {
   normal: {
@@ -11,5 +12,7 @@ const s = {
 };
 
 storiesOf('CofirmBtn', module).add('normal', () =>
-  <ConfirmBtn style={s.normal} test="22"> test </ConfirmBtn>,
+  <ConfirmBtn style={s.normal} test="22" onConfirm={action('Confirmed!!!')}>
+    Delete
+  </ConfirmBtn>,
 );
