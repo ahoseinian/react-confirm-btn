6defe7cf156e024e81f38ee80b0103f578418f8d
Amir Hoseinian <amir.anyware@gmail.com>
2017-06-09 10:31:19 +0430

    Update stories to show props

--- stories/index.js
+++ stories/index.js
@@ -14,21 +14,76 @@ const s = {
     padding: '1rem 2rem',
     cursor: 'pointer',
   },
+  pre: {
+    background: '#2c3e50',
+    padding: '1rem',
+    width: '25rem',
+    color: '#ecf0f1',
+    position: 'absolute',
+    bottom: 0,
+  },
 };
 
+function replacer(key, value) {
+  // Filtering out properties
+  if (typeof value === 'function') {
+    return 'Function';
+  }
+  return value;
+}
+
+const PrintProps = props =>
+  <pre style={s.pre}>
+    {JSON.stringify(props, replacer, 2)}
+  </pre>;
+
 storiesOf('CofirmBtn', module)
-  .add('normal', () =>
-    <ConfirmBtn onConfirm={action('Confirmed!!!')}>
-      Delete
-    </ConfirmBtn>,
-  )
-  .add('with style', () =>
-    <ConfirmBtn style={s.withStyle} onConfirm={action('Confirmed!!!')}>
-      Delete
-    </ConfirmBtn>,
-  )
-  .add('with className', () =>
-    <ConfirmBtn className="btn" onConfirm={action('Confirmed!!!')}>
-      Delete
-    </ConfirmBtn>,
-  );
+  .add('Normal', () => {
+    const props = {
+      onConfirm: action('Confirmed!!!'),
+    };
+    return (
+      <div>
+        <ConfirmBtn {...props}> Delete </ConfirmBtn>
+        <PrintProps props={props} />
+      </div>
+    );
+  })
+  .add('With style', () => {
+    const props = {
+      style: s.withStyle,
+      onConfirm: action('Confirmed!!!'),
+    };
+    return (
+      <div>
+        <ConfirmBtn {...props}> Delete </ConfirmBtn>
+        <PrintProps props={props} />
+      </div>
+    );
+  })
+  .add('With className', () => {
+    const props = {
+      className: 'string',
+      onConfirm: action('Confirmed!!!'),
+    };
+
+    return (
+      <div>
+        <ConfirmBtn {...props}> Delete </ConfirmBtn>
+        <PrintProps props={props} />
+      </div>
+    );
+  })
+  .add('With custom confirmation text', () => {
+    const props = {
+      className: 'btn',
+      onConfirm: action('Confirmed!!!'),
+      confirmText: 'Stop !!!',
+    };
+    return (
+      <div>
+        <ConfirmBtn {...props}> Delete </ConfirmBtn>
+        <PrintProps props={props} />
+      </div>
+    );
+  });
